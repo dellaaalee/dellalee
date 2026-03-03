74721fb153457245e11c54c2975772c99c46646c
Della Lee <dellalee@Dellas-MacBook-Air.local>
2026-03-02 18:25:46 -0800

    deloy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dellalee/', // <-- IMPORTANT (example: '/della/')
+  base: '/dellalee/',
 })
